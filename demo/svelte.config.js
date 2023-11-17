f94933d198d43f32b17986caa5618422537a6b31
jill64 <intents.turrets0h@icloud.com>
2023-11-18 03:07:55 +0900

    fix: site adapter

--- demo/svelte.config.js
+++ demo/svelte.config.js
@@ -1,11 +1,20 @@
-import adapter from '@sveltejs/adapter-auto'
+import cloudflare from '@sveltejs/adapter-cloudflare'
+import netlify from '@sveltejs/adapter-netlify'
+import vercel from '@sveltejs/adapter-vercel'
+import auto from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+
+const url = process.env.PREVIEW_URL
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: url.includes('netlify.app') ? netlify({
+      edge: true
+    }) : url.includes('vercel.app') ? vercel({
+      edge: true
+    }) : url.includes('pages.dev') ? cloudflare() : auto(),
     prerender: {
       crawl: false
     },
