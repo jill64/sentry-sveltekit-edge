25f160ea6603088a864a1781de6f80d56a107a85
jill64 <intents.turrets0h@icloud.com>
2023-11-17 12:14:06 +0900

    setup

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  use: {
+    baseURL: process.env.PREVIEW_URL
+  },
+  testDir: 'tests',
+  fullyParallel: true,
+  workers: '100%',
+  retries: process.env.CI ? 2 : 0,
+  projects: [
+    {
+      name: 'chromium',
+      use: devices['Desktop Chrome']
+    },
+    {
+      name: 'firefox',
+      use: devices['Desktop Firefox']
+    },
+    {
+      name: 'webkit',
+      use: devices['Desktop Safari']
+    },
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5']
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12']
+    }
+  ]
+})
