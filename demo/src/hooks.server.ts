c179213aaafbe717d9d0201a5277c1212e2ca7e9
jill64 <intents.turrets0h@icloud.com>
2023-12-08 01:51:33 +0900

    update demo

--- demo/src/hooks.server.ts
+++ demo/src/hooks.server.ts
@@ -1,8 +1,19 @@
-import { serverInit } from '$dist/index.js'
+import { serverInit } from '$dist'
 
 const { onHandle, onError } = serverInit(
   'https://66e05c5ea6c42d35a76741e9420abcfb@o4505814639312896.ingest.sentry.io/4506235746516992'
 )
 
-export const handle = onHandle()
-export const handleError = onError()
+export const handle = onHandle(({ event, resolve }) => {
+  console.log(`${event.request.method} ${event.url.pathname}`)
+  return resolve(event)
+})
+
+export const handleError = onError((e, sentryEventId) => {
+  console.error(e)
+
+  return {
+    message: 'This error was successfully sent to Sentry',
+    sentryEventId
+  }
+})
