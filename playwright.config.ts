17339b24f1cfed0b202b4f5cd410d6f2c8243c7b
jill64 <intents.turrets0h@icloud.com>
2023-12-09 20:29:09 +0900

    Update CI workflow and playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,3 @@
 import { extendsConfig, branchPreview } from '@jill64/playwright-config'
 
-export default extendsConfig(branchPreview(process.env.HOSTING_PROVIDER))
+export default extendsConfig(branchPreview())
